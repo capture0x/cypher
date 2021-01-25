e8162ceaf8aa60f853f219997b96429feff2bc30
capture0x <61238348+capture0x@users.noreply.github.com>
2021-01-25 21:53:25 +0000

    Add files via upload

---
+++ imcdec.py
@@ -0,0 +1,22 @@
+from Crypto.Cipher import AES
+
+
+def decrypt_image():
+    try:
+        key = input("Enter Your Key :")
+        iv = input("Enter Your IV :")
+        file_name = input("Enter Encrypt Image Path :")
+        cfb_cipher = AES.new(key.encode(), AES.MODE_CFB,iv.encode())
+        input_file = open(file_name, "rb")
+        input_data = input_file.read()
+        input_file.close()
+        enc_data = cfb_cipher.decrypt(input_data)
+        enc_file = open(file_name.replace(".wrr", ""), "wb")
+        enc_file.write(enc_data)
+        enc_file.close()
+        print("Decrypted :", file_name)
+
+    except:
+        print("Key and iv value must be 16 bytes...")
+
+decrypt_image()
